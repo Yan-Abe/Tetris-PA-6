6ce9f3b54be45d18a9c2eea59fd43b412a12949d
EnricoConto <conto.enrico@gmail.com>
2021-10-21 22:01:09 -0300

    Criado módulo view para manipular a interface

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
+import TelaJogo from './src/view.js';
 
-const Campo = (elemento, w, h )=>{
-    console.log("#1")
-    this.linhas = 20
-    this.colunas = 10
+const telaJogo = new TelaJogo(document.getElementById("app"), 480, 640);
 
-    // largura do campo
-    this.width = w
-    this.height = h
-    this.element = elemento;
+function criarCampoDeJogo() {
+  const campoDeJogo = [];
+  for (let y = 0; y < 20; y++) {
+    campoDeJogo[y] = [];
+    for (let x = 0; x < 10; x++) {
+      campoDeJogo[y][x] = 1;
+    }
+  }
+  return campoDeJogo;
+}
 
-    this.canvas = document.createElement("canvas");
-    this.context = this.canvas.getContext("2d");
+const gameStatus = { level: 1, score:0, lines:0 , campoDeJogo: criarCampoDeJogo()}
 
-    // largura do campo
-    this.canvas.width = this.width
-    this.canvas.height = this.height
-
-    // Largura da borda do campo de jogo
-    this.larguraBordaTela = 4;
-
-    this.larguraBordaTela = 4;
-    this.telaX = this.larguraBordaTela;
-    this.telaY = this.larguraBordaTela;
-    this.telaWidth = (this.width * 2) / 3;
-    this.telaHeight = this.height;
-    this.telaWidthInterno =
-      this.telaWidth - this.larguraBordaTela * 2;
-    this.telaHeightInterno =
-      this.telaHeight - this.larguraBordaTela * 2;
-
-    this.blockWidth = this.telaWidthInterno / linhas;
-    this.blockHeight = this.telaHeightInterno / colunas;
-
-    this.painelX = this.telaWidth + 10;
-    this.painelY = 0;
-    this.painelWidth = this.width / 3;
-    this.painelHeight = this.height;
-
-    this.element.appendChild(this.canvas);
-    console.log("#1")
-};
-
-
-const campo = Campo(document.getElementById("app"), 480, 640);
+telaJogo.renderCampoJogo(gameStatus);
+telaJogo.renderPainelLateral(gameStatus);
